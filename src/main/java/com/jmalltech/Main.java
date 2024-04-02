594f40c13486abaf0ba7edabcdbcdae152318291
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-02 19:33:53 -0400

    Security-Library 1.1 stable

--- src/main/java/com/jmalltech/Main.java
+++ src/main/java/com/jmalltech/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@MapperScan(basePackages = {"com.jmalltech.mapper","com.jmalltech.security.mapper"})
+@MapperScan(basePackages = {"com.jmalltech.mapper"})
 @EnableCaching
 @SpringBootApplication
 //@EnableDiscoveryClient
