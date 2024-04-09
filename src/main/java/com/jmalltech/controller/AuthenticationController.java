c7fbe433094917eb1d6a33f9231e0a0705c2afa1
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-09 12:42:25 -0400

    @CrossOriginal added to ClientController and AuthenticationController

--- src/main/java/com/jmalltech/controller/AuthenticationController.java
+++ src/main/java/com/jmalltech/controller/AuthenticationController.java
@@ -10,11 +10,13 @@ import com.jmalltech.util.TokenResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
     @Autowired
     private ClientDomainService clientDomainService;
@@ -40,7 +42,7 @@ public class AuthenticationController {
         if(user != null){
             String token = JwtUtil.generateToken(user, role);
             System.out.println("Generated token: " + token);
-            return ResponseEntity.ok(new TokenResponse(token));
+            return ResponseEntity.ok(new TokenResponse(token, role));
         }else {
             System.out.println("Login failed for username: " + loginRequest.getUsername());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
--- src/main/java/com/jmalltech/controller/ClientController.java
+++ src/main/java/com/jmalltech/controller/ClientController.java
@@ -3,18 +3,31 @@ package com.jmalltech.controller;
 import com.jmalltech.entity.Client;
 import com.jmalltech.helper.ResponseHelper;
 import com.jmalltech.service.ClientDomainService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/clients")
+@CrossOrigin(origins = "http://localhost:4200") // 允许http://localhost:4200来源的跨源请求
 public class ClientController {
     private ClientDomainService clientService;
 
     @Autowired
     public ClientController(ClientDomainService clientService) {
         this.clientService = clientService;
+    }
+
+    @GetMapping("/my-info")
+    public ResponseEntity<?> geMyInfo(HttpServletRequest request){
+        String username = (String) request.getAttribute("username");
+        Client client = clientService.getByUsername(username);
+        if (client != null) {
+            return ResponseEntity.ok(client);
+        } else {
+            return ResponseHelper.notFoundResponse("Client not found for username: " + username);
+        }
     }
 
     @GetMapping("/by-id/{id}")
